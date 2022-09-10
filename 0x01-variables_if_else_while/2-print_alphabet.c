aadba28662afef283b047fe460431c8c5169c8dd
ChukkyObi <ernestchuks243@gmail.com>
2022-09-09 17:35:32 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,18 +5,18 @@
  * Return: alway returns 0
  *
  */
-int main(void)  
+int main(void)
 
 {
 char alphabet;
 
 for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
-{    
+{
 putchar(alphabet);
 }
 
 putchar('\n');
-  
+
 return (0);
 
 }
