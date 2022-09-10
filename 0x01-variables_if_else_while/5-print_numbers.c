3ded985c1018728f82d18b478ace7cddeefbf110
ChukkyObi <ernestchuks243@gmail.com>
2022-09-09 19:03:07 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always return 0
+ *
+ */
+int main(void)
+
+{
+int digit;
+for (digit = 0 ; digit < 10; digit++)
+{
+printf("%digit", digit);
+}
+
+printf("\n");
+
+return (0);
+
+}
