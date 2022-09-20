ec0d1c5ca4cb2b4ff669e8fed832d50eecbdb13a
ChukkyObi <ernestchuks243@gmail.com>
2022-09-20 05:07:26 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -23,6 +23,8 @@ int sign;
 sign = 1;
 number = 0;
 
+
+
 for (i = 0; s[i] != '\0'; i++)
 {
 if (is_numerical(s[i]))
@@ -32,7 +34,7 @@ number = (s[i] - 48) + number * 10;
 if (s[i + 1] == ' ')
 break;
 }
-else if (s[i] == '_')
+else if (s[i] == '-')
 {
 sign *= -1;
 }
@@ -40,5 +42,6 @@ sign *= -1;
 }
 
 return (number *sign);
+
 
 }
