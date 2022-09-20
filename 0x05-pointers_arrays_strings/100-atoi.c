5451a0c8f79e9f585e3b0fa33e5a3fcccab5abed
ChukkyObi <ernestchuks243@gmail.com>
2022-09-20 04:50:33 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * is_numerical - check if it is a digit
  * @n: Number
@@ -8,6 +9,7 @@ int is_numerical(unsigned int n)
 {
 return (n >= '0' &&  n <= '9');
 }
+
 /**
  * _atoi - convert a string to an integer
  *@s: String
@@ -20,11 +22,13 @@ int sign;
 
 sign = 1;
 number = 0;
+
 for (i = 0; s[i] != '\0'; i++)
 {
 if (is_numerical(s[i]))
 {
 number = (s[i] - 48) + number * 10;
+
 if (s[i + 1] == ' ')
 break;
 }
@@ -32,6 +36,9 @@ else if (s[i] == '_')
 {
 sign *= -1;
 }
+
 }
+
 return (number *sign);
+
 }
